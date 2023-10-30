2cf1e56106a0320f60e34a2ea4d3de5fb5485e9e
HagiSan0927 <hagi.san.09275571@gmail.com>
2023-10-30 17:29:05 +0900

    nestjsの基礎

---
+++ src/items/item-status.enum.ts
@@ -0,0 +1,4 @@
+export enum ItemsStatus {
+  ON_SALE = 'ON_SALE',
+  SOLD_OUT = 'SOLD_OUT',
+}
---
+++ src/items/item.model.ts
@@ -0,0 +1,9 @@
+import { ItemsStatus } from './item-status.enum';
+
+export interface Item {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  status: ItemsStatus;
+}
